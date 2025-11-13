27ceec1744451ea8ac6bf80f6f921ab910529b9a
noc-turne-git <Nganlb.working@gmail.com>
2025-11-13 17:31:04 +0700

    Update

---
+++ CloudGame/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace CloudGame
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        // App.xaml.cs
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+        }
+
+    }
+
+}
---
+++ CloudGame/Core/GameTime.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudGame.Core
+{       
+    internal class GameTime
+    {
+        public TimeSpan DeltaTime { get; set; }
+    }
+}
